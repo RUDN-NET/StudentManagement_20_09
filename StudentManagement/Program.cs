c9a86db79eff8a43f49cd8703ef055f85b0759db
Infarh <shmachilin@yandex.ru>
2020-09-29 09:22:45 +0300

    Добавьте файлы проекта.

---
+++ StudentManagement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace StudentManagement
+{
+    class Program
+    {
+        private const string __NamesFile = "Names.txt";
+
+        static void Main(string[] args)
+        {
+            using (StreamReader names_reader = new StreamReader(__NamesFile))
+            {
+                while (!names_reader.EndOfStream)
+                {
+                    var line = names_reader.ReadLine();
+
+                    Console.WriteLine(line);
+                }
+
+                //names_reader.Close();
+                //names_reader.Dispose();
+            }
+
+
+            Console.WriteLine("Нажмите Enter для выхода");
+            Console.ReadLine();
+        }
+    }
+}
