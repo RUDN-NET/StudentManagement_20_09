1d5b5a3c6b1db8cdcf318f5c56d222bb697ac808
Infarh <shmachilin@yandex.ru>
2020-09-29 10:18:37 +0300

    Сортировка массива студентов по среднему рейтингу по убыванию

--- StudentManagement/Program.cs
+++ StudentManagement/Program.cs
@@ -15,10 +15,82 @@ namespace StudentManagement
 
             var students = GetStudents(surnames, names, patronymics, 1000);
 
+            const string students_data_file = "students.csv";
+            SaveToFile(students, students_data_file);
 
+            var students2 = ReadFromFile(students_data_file);
+
+            var student = students2[0];
+
+            //double rating = student;
+            //double rating2 = (double)student;
+
+            SortStudentsByAverageRating(students2);
+
+            for (var i = 0; i < students2.Length; i++)
+                Console.WriteLine(students2[i]);
 
             Console.WriteLine("Нажмите Enter для выхода");
             Console.ReadLine();
+        }
+
+        private static void SaveToFile(Student[] Students, string FileName)
+        {
+            using (var writer = new StreamWriter(FileName))
+            {
+                for (var i = 0; i < Students.Length; i++)
+                {
+                    var student = Students[i];
+                    writer.WriteLine("{0};{1};{2};{3}",
+                        student.Surname, student.Name, student.Patronymic,
+                        string.Join(";", student.Ratings));
+                }
+            }
+        }
+
+        private static void SortStudentsByAverageRating(Student[] Students)
+        {
+            Array.Sort(Students, (s1, s2) =>
+            {
+                var rating1 = s1.RatingsAverage;
+                var rating2 = s2.RatingsAverage;
+                if (rating1 > rating2) return -1;
+                else if (rating1 < rating2) return +1;
+                else return 0;
+            });
+        }
+
+        private static Student[] ReadFromFile(string FileName)
+        {
+            using (var reader = new StreamReader(FileName))
+            {
+                var students = new List<Student>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if(string.IsNullOrWhiteSpace(line)) 
+                        continue;
+
+                    var components = line.Split(";");
+                    var student = new Student();
+                    student.Surname = components[0];
+                    student.Name = components[1];
+                    student.Patronymic = components[2];
+
+                    var ratings_count = components.Length - 3;
+
+                    var ratings = new List<int>(ratings_count);
+                    for(var i = 0; i < ratings_count; i++)
+                        ratings.Add(int.Parse(components[i + 3]));
+
+                    student.Ratings = ratings;
+
+                    students.Add(student);
+                }
+
+                return students.ToArray();
+            }
         }
 
         private static Student[] GetStudents(List<string> Surnames, List<string> Names, List<string> Patronymics, int Count)
--- StudentManagement/Student.cs
+++ StudentManagement/Student.cs
@@ -12,9 +12,46 @@ namespace StudentManagement
 
         public List<int> Ratings { get; set; } = new List<int>();
 
+        public int RatingsCount
+        {
+            get
+            {
+                return Ratings.Count;
+            }
+        }
+
+        public int RatingsSum
+        {
+            get
+            {
+                var sum = 0;
+                for (var i = 0; i < RatingsCount; i++)
+                    sum += Ratings[i];
+                return sum;
+            }
+        }
+
+        public double RatingsAverage
+        {
+            get
+            {
+                return (double)RatingsSum / RatingsCount;
+            }
+        }
+
         public override string ToString()
         {
-            return $"Студент {Surname} {Name} {Patronymic}: {string.Join(", ", Ratings)}";
+            return $"Студент {Surname} {Name} {Patronymic} ({RatingsAverage:f3}): {string.Join(", ", Ratings)}";
+        }
+
+        //public static implicit operator double(Student student) // Оператор неявного преобразования типа данных "Студент" в тип данных "вещественное число двойной точности"
+        //{
+        //    return student.RatingsAverage;
+        //}
+
+        public static explicit operator double(Student student) // Оператор явного преобразования типа данных "Студент" в тип данных "вещественное число двойной точности"
+        {
+            return student.RatingsAverage;
         }
     }
 }
