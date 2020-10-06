58194f6d4a9caa0acb51200e8278723e2ac0541d
Infarh <shmachilin@yandex.ru>
2020-10-06 10:51:14 +0300

    Работа с дирректориями

--- StudentManagement/Program.cs
+++ StudentManagement/Program.cs
@@ -6,6 +6,7 @@ namespace StudentManagement
     class Program
     {
         private const string __FileName = "Names.txt";
+        private const string __TestDir = "TestDir";
 
         private static void TestFileInfo()
         {
@@ -46,12 +47,91 @@ namespace StudentManagement
             }
         }
 
+        private static void TestDirectoryInfo()
+        {
+            var current_dir = new DirectoryInfo(".");
+            var current_dir2 = new DirectoryInfo(Environment.CurrentDirectory);
+
+            //foreach (var json_file in current_dir.GetFiles("*.json"))
+            //{
+            //    Console.WriteLine(json_file);
+            //}
+
+            //Console.ReadLine();
+
+            var test_dir = new DirectoryInfo(__TestDir);
+
+            //test_dir.Delete(true);
+
+            //foreach (var file in test_dir.GetFiles())
+            //    Console.WriteLine("{0} - {1:f2}кБ", file.Name, file.Length / 1024.0);
+
+            PrintFileOfDir(test_dir);
+
+            Console.WriteLine("Полная длина файлов во вложенных директориях {0:f3}Б",
+                GetTotalFilesLength(test_dir));
+
+            RenameFilesAndDirs(test_dir);
+        }
+
+        private static void PrintFileOfDir(DirectoryInfo dir)
+        {
+            Console.WriteLine(dir);
+            foreach (var file in dir.GetFiles())
+                Console.WriteLine("{0} - {1:f2}кБ", file.Name, file.Length / 1024.0);
+            Console.WriteLine();
+
+            foreach (var sub_dir in dir.GetDirectories())
+                PrintFileOfDir(sub_dir);
+        }
+
+        private static long GetTotalFilesLength(DirectoryInfo dir)
+        {
+            var length = 0l;
+
+            foreach (var file in dir.GetFiles())
+                length += file.Length;
+
+            foreach (var sub_dir in dir.GetDirectories())
+                length += GetTotalFilesLength(sub_dir);
+
+            return length;
+        }
+
+        private static void RenameFilesAndDirs(DirectoryInfo dir)
+        {
+            var file_template = $"File{{0}}[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].txt";
+            var index = 0;
+            foreach (var file in dir.GetFiles())
+            {
+                var new_file_name = dir.FullName + "\\" + string.Format(file_template, index++);
+                Console.WriteLine("Переношу содержимое файла");
+                Console.WriteLine("\tиз {0}", file.FullName);
+                Console.WriteLine("\tв {0}", new_file_name);
+                file.MoveTo(new_file_name);
+            }
+
+            var dir_template = $"Dir{{0}}[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}]";
+            index = 0;
+            foreach (var sub_dir in dir.GetDirectories())
+            {
+                RenameFilesAndDirs(sub_dir);
+                var new_dir_name = dir.FullName + "\\" + string.Format(dir_template, index++);
+                Console.WriteLine("Переношу содержимое директории");
+                Console.WriteLine("\tиз {0}", sub_dir.FullName);
+                Console.WriteLine("\tв {0}", new_dir_name);
+                sub_dir.MoveTo(new_dir_name);
+            }
+        }
+
         static void Main(string[] args)
         {
-            TestFileInfo();
+            //TestFileInfo();
+            TestDirectoryInfo();
 
             Console.WriteLine("Нажмите Enter для выхода");
             Console.ReadLine();
         }
+
     }
 }
